e2e03928a93ac018a3203c564bab0a07aabb4c13
Leyang Pan <65788181+YuanSu526@users.noreply.github.com>
2024-03-23 15:23:04 -0700

    setting up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Card from './components/Card';
+import Home from './components/Home/Home';
+
 
 function App() {
   return (
-    <div className = 'App'>
-      <h1>Welcome to PromptCorn!</h1>
-
-      <Card
-        title="Example Card"
-        description="This is a sample card component."
-        imageUrl={logo}
-      />
-    </div>
-   
+    <Router>
+      <Routes>
+        {/* <Route path="/Search" element={<Search />} /> */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 

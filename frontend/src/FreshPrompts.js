731e2a357fc4ad2c9767afc4cef7650a4c6976d4
danee <danethan@students.cs.ubc.ca>
2024-03-23 18:17:10 -0700

    basic scroll

---
+++ frontend/src/FreshPrompts.js
@@ -0,0 +1,17 @@
+import Card from "./components/Card";
+function FreshPrompts() {
+    return (
+        <div class = "container">
+            <Card title = "Fresh 1" description = "So Fresh"/>
+            <Card title = "Fresh 1" description = "So Fresh"/>
+            <Card title = "Fresh 1" description = "So Fresh"/>
+            <Card title = "Fresh 1" description = "So Fresh"/>
+            <Card title = "Fresh 1" description = "So Fresh"/>
+
+        </div>
+
+    );
+
+}
+
+export default FreshPrompts;
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Home.css";
 import Header from '../Header';
+import FreshPrompts from "../../FreshPrompts";
 
 function Home() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <Header/>
-      <h1>PromptCorn</h1>
-      <p>Tadaaaaaa</p>
-=======
 
   return (
     <div id="main">
-      
->>>>>>> 8658729aab330b4562a539fd19ea12a32ac4a2d9
+      <Header/>
+      <FreshPrompts/>
     </div>
   );
 

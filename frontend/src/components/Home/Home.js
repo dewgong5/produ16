f8ee56d9bc75c8a13614d57b36f52c27e8bfc08b
danee <danethan@students.cs.ubc.ca>
2024-03-23 19:44:23 -0700

    help

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Header from '../Header';
 import FreshPrompts from "../../FreshPrompts";
+import Hotprompts from "../Hotprompts";
 
 function Home() {
 
   return (
     <div id="main">
       <Header/>
+      <Hotprompts/>
       <FreshPrompts/>
     </div>
   );

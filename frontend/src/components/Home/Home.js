f8232e89304734e32954c1681707b8bc468edd01
danee <danethan@students.cs.ubc.ca>
2024-03-23 19:59:14 -0700

    t

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Header from '../Header';
 import FreshPrompts from "../../FreshPrompts";
 import Hotprompts from "../Hotprompts";
 
 function Home() {
+  var [hide, setHide] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log("Handle scroll1")
+      if (window.scrollY > 10) {
+        setHide(true);
+        console.log("Handle scroll2")
+
+      } else {
+        setHide(false);
+        console.log("Handle scroll3")
+
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div id="main">
-      <Header/>
-      <Hotprompts/>
-      <FreshPrompts/>
+      <Header />
+      {!hide ? <Hotprompts /> : null}
+      <FreshPrompts />
     </div>
   );
-
 }
 
 export default Home;
